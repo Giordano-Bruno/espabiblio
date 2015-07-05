34963bba5262ca9a12281cbcfbcec0fb2029c948
joanlaga <joanlaga@hotmail.com>
2015-07-04 21:27:10 -0500

    se integran las traducciones originales del idioma ingles para los campos MARC

--- locale/en/sql/GB-0.33/MARC/usmarc_block_dm.sql
+++ locale/en/sql/GB-0.33/MARC/usmarc_block_dm.sql
@@ -1,11 +1,11 @@
-TRUNCATE  %prfx%usmarc_block_dm;
-insert into %prfx%usmarc_block_dm values (0,'Números de control bibliográfico e información codificada');
-insert into %prfx%usmarc_block_dm values (1,'Entradas principales');
-insert into %prfx%usmarc_block_dm values (2,'Información sobre títulos, edición y sello editorial, etc.');
-insert into %prfx%usmarc_block_dm values (3,'Descripción física, etc.');
-insert into %prfx%usmarc_block_dm values (4,'Menciones de series');
-insert into %prfx%usmarc_block_dm values (5,'Notas');
-insert into %prfx%usmarc_block_dm values (6,'Entradas de acceso por tema');
-insert into %prfx%usmarc_block_dm values (7,'Entradas agregadas que no son de tema ni series y campos de enlace');
-insert into %prfx%usmarc_block_dm values (8,'Entradas agregadas de series y existencias');
-insert into %prfx%usmarc_block_dm values (9,'Campos para uso local');
+insert into %prfx%usmarc_block_dm values (0,'Control information, numbers, and codes');
+insert into %prfx%usmarc_block_dm values (1,'Main entry');
+insert into %prfx%usmarc_block_dm values (2,'Titles and title paragraph (title, edition, imprint)');
+insert into %prfx%usmarc_block_dm values (3,'Physical description, etc.');
+insert into %prfx%usmarc_block_dm values (4,'Series statements');
+insert into %prfx%usmarc_block_dm values (5,'Notes');
+insert into %prfx%usmarc_block_dm values (6,'Subject access fields');
+insert into %prfx%usmarc_block_dm values (7,'Added entries other than subject or series, linking fields');
+insert into %prfx%usmarc_block_dm values (8,'Series added entries: location, and alternate graphics');
+insert into %prfx%usmarc_block_dm values (9,'Reserved for local implementation');
+
